37ec2e8ffb76f81ca6ed5b81c7ce45331da8aa95
yashas1810 <brjeevan26@gmail.com>
2026-02-06 10:09:52 +0530

    Add files via upload

---
+++ sql1.sql
@@ -0,0 +1,8 @@
+use yashas;
+create table employee(
+id int primary key,
+name varchar(45)
+)
+select * from employee;
+insert into employee values(1,"yashas")
+insert into employee values(2,"riya")
